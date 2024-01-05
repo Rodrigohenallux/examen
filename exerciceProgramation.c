30e2386a86f6dafc7f33d4581966a9aaaf7550cd
AmineDarrazi <aminedrz18@gmail.com>
2024-01-05 18:30:42 +0100

    exerciceProgramation.c

---
+++ exerciceProgramation.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main(void){
+
+	
+	/*
+	int chiffre[20];
+
+	for(int i = 0 ; i < 21 ; i++){
+		
+		chiffre[i] = i; 
+
+		printf("%d - ", chiffre[i]);
+	}
+	*/
+
+	int chiffre[20];
+
+	for(int i = 1 ; i < 21 ; i++){
+		chiffre
+	}
+
+}
